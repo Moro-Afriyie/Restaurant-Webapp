6bb9de85ee6b26b200acb40cf8255889d8fffba3
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-30 09:30:52 -0100

    style the payment erro snackbar

--- restaurant-website/src/app/order-page/order-page.component.ts
+++ restaurant-website/src/app/order-page/order-page.component.ts
@@ -56,7 +56,7 @@ export class OrderPageComponent implements OnInit {
   ) {}
   number = '233501658639';
   url = 'https://restaurant-payment-backend.herokuapp.com/api/payment';
-  paymentError = true;
+  paymentError = false;
   submitted = false;
   error = 'An unexpected error occured. Please try again';
 
@@ -129,5 +129,9 @@ export class OrderPageComponent implements OnInit {
           (err) => reject(err)
         );
     });
+  }
+
+  onClose(): void {
+    this.paymentError = false;
   }
 }
