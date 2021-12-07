e01827f294acd671c516ebff8eb0a868ded7a972
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-07 10:39:33 -0100

    edit the id's

--- restaurant-website/src/app/services/socket-service.service.ts
+++ restaurant-website/src/app/services/socket-service.service.ts
@@ -9,42 +9,42 @@ export class SocketService {
   success: boolean = false;
   foodArray: Food[] = [
     {
-      id: '33cc84ae-bc4b-49b9-bdc1-81782680c493',
+      id: '33cc84aebc4b49b9bdc181782680c493',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
       price: '80.00',
     },
     {
-      id: '3646754e-1057-4da3-a16a-90e2ecff5e06',
+      id: '3646754e10574da3a16a90e2ecff5e06',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
       price: '40.00',
     },
     {
-      id: '4226d4f1-e91e-4048-8034-5bc18be88e5b',
+      id: '4226d4f1e91e404880345bc18be88e5b',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
       price: '30.00',
     },
     {
-      id: 'ab62ad68-aff4-43af-a4c8-27a78a22e3a3',
+      id: 'ab62ad68aff443afa4c827a78a22e3a3',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
       price: '50.00',
     },
     {
-      id: '6fe15e03-186f-478b-8c23-99ae70a51960',
+      id: '6fe15e03186f478b8c2399ae70a51960',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
       price: '70.00',
     },
     {
-      id: 'c4d3ddc8-86c5-4014-9323-387915598847',
+      id: 'c4d3ddc886c540149323387915598847',
       body: 'Lorem ipsum dolor sit amet consectetur',
       image: '../../assets/pizza2.jpg',
       alt: 'pizza',
