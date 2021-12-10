2772561d379814ba5b6709580703e3839c25a691
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-10 10:05:29 -0100

    set up the routes

--- admin-page/src/app/app-routing.module.ts
+++ admin-page/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { DisplayPageComponent } from './display-page/display-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DisplayPageComponent },
+  { path: 'orders', component: DisplayPageComponent },
+  { path: 'delivered', component: DisplayPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
