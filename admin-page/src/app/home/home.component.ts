0d5ff12787ef86e15a9f59be9bf2a0ad3edd2642
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-10 11:09:50 -0100

    add a logic to route to the orders page when the page loads

--- admin-page/src/app/home/home.component.ts
+++ admin-page/src/app/home/home.component.ts
@@ -1,15 +1,15 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: Router) {}
 
   ngOnInit(): void {
+    this.route.navigate(['/orders']);
   }
-
 }
