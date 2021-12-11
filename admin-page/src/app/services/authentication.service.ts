2806bf1647de9b6c1513ce2c7ffb73e6ec6b7d7d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-11 08:58:11 -0100

    done with the login authentication

--- admin-page/src/app/services/authentication.service.ts
+++ admin-page/src/app/services/authentication.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
-  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore) {}
+  constructor(public afAuth: AngularFireAuth) {}
 
   /* Sign in */
   logIn(email: string, password: string) {
