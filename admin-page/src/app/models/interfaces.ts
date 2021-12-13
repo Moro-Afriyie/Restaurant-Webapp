625ea8b4f8b32032189b8b6a47eae51081a415e4
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-13 12:00:28 -0100

    create orderDetails interface

---
+++ admin-page/src/app/models/interfaces.ts
@@ -0,0 +1,10 @@
+export interface OrderDetails {
+  date: string;
+  orderNumber: number;
+  name: string;
+  foodOrdered: string;
+  phoneNumber: string;
+  location: string;
+  Price: string;
+  completed: boolean;
+}
