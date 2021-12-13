c89fcc9730e2895f40cf726ad83165cabdd3885e
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-13 11:47:40 -0100

    implement the logout route

--- admin-page/src/app/sidebar/sidebar.component.ts
+++ admin-page/src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,26 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logOut(): void {
+    this.authService
+      .logOut()
+      .then((res) => {
+        this.router.navigate(['/login']);
+      })
+      .catch((err) => console.log(err));
   }
-
 }
