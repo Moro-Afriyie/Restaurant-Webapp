f1ff6f56297b6e7ebd8edd03707b2325453baf22
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-14 09:31:08 -0100

    add functions to query the documents where completed == false

--- admin-page/src/app/completed-orders/completed-orders.component.ts
+++ admin-page/src/app/completed-orders/completed-orders.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+import { OrderDetails } from '../models/interfaces';
 
 @Component({
   selector: 'app-completed-orders',
   templateUrl: './completed-orders.component.html',
-  styleUrls: ['./completed-orders.component.scss']
+  styleUrls: ['./completed-orders.component.scss'],
 })
 export class CompletedOrdersComponent implements OnInit {
+  item$: Observable<OrderDetails[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private firestore: AngularFirestore) {
+    this.item$ = this.GetCompletedOrdersCollection();
   }
 
+  ngOnInit(): void {
+    this.firestore
+      .collection('orders', (orders) => orders.where('completed', '==', true))
+      .valueChanges()
+      .subscribe((res) => console.log(res));
+  }
+
+  GetCompletedOrdersCollection(): Observable<any> {
+    return this.firestore
+      .collection('orders', (orders) => orders.where('completed', '==', true))
+      .valueChanges();
+  }
 }
