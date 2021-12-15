0b9e2d160dfa304295f47bd7db10ec05a7397e8a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-15 15:18:24 -0100

    create the routes for the various components

--- restaurant-website/src/app/app-routing.module.ts
+++ restaurant-website/src/app/app-routing.module.ts
@@ -3,11 +3,22 @@ import { OrderPageComponent } from './order-page/order-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { DisplayPageComponent } from './display-page/display-page.component';
+import { CompletedOrdersComponent } from './completed-orders/completed-orders.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'orders/:id', component: OrderPageComponent },
-  { path: 'admin', component: AdminComponent, children: [] },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      { path: 'orders', component: DisplayPageComponent },
+      { path: 'delivered', component: CompletedOrdersComponent },
+    ],
+  },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
