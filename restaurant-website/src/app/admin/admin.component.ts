de26264c1abb36b4f6ff383def725a4a20c7f37f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-15 15:26:49 -0100

    done with the sidebar

--- restaurant-website/src/app/admin/admin.component.ts
+++ restaurant-website/src/app/admin/admin.component.ts
@@ -1,15 +1,15 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
+    this.router.navigate(['/admin/orders']);
   }
-
 }
--- restaurant-website/src/app/sidebar/sidebar.component.ts
+++ restaurant-website/src/app/sidebar/sidebar.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logOut(): void {
+    // this.authService
+    //   .logOut()
+    //   .then((res) => {
+    //     this.router.navigate(['/login']);
+    //   })
+    //   .catch((err) => console.log(err));
   }
-
 }
