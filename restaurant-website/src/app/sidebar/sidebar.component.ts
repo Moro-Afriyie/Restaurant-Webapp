ac7d79d0880ac5be4c44fb81e7dd57aff9f4326a
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-12-15 15:51:12 -0100

    implement the logout function

--- restaurant-website/src/app/sidebar/sidebar.component.ts
+++ restaurant-website/src/app/sidebar/sidebar.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,16 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
 
   logOut(): void {
-    // this.authService
-    //   .logOut()
-    //   .then((res) => {
-    //     this.router.navigate(['/login']);
-    //   })
-    //   .catch((err) => console.log(err));
+    this.authService
+      .logOut()
+      .then((res) => {
+        this.router.navigate(['/login']);
+      })
+      .catch((err) => console.log(err));
   }
 }
