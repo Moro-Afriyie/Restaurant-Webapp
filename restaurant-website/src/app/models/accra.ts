254e5793071bc033bc6ea0d7258eae7cf1cdb453
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-09 22:29:05 -0100

    add the towns and theeir delivery prices

--- restaurant-website/src/app/models/accra.ts
+++ restaurant-website/src/app/models/accra.ts
@@ -1,1 +1,11 @@
-export const cities = [{}];
+export const cities = [
+  { name: 'Accra Metropolitan ', price: '' },
+  { name: 'Achimota', price: '' },
+  { name: 'Adabraka', price: '' },
+  { name: 'Adenta', price: '' },
+  { name: 'Tema Metropolitan', price: '' },
+  { name: 'Abelemkpe', price: '' },
+  { name: 'Ablekuma', price: '' },
+  { name: 'Abofu', price: '' },
+  { name: 'Abokobi', price: '' },
+];
