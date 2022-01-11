2576c7c9013f310e24516fdbca6d588bcf38a8ba
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-11 16:07:45 -0100

    add a snackbar to show when orders are closed so the admin can open it

--- restaurant-website/src/app/admin/admin.component.ts
+++ restaurant-website/src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-  constructor(private router: Router) {}
+  orderStatus = false;
+  closeOrder = false;
+  private socket: any;
+  constructor(private router: Router, private http: HttpClient) {
+    this.socket = io('https://restaurant-payment-backend.herokuapp.com/');
+  }
 
   ngOnInit(): void {
     this.router.navigate(['/admin/orders']);
+    this.http
+      .get('https://restaurant-payment-backend.herokuapp.com/')
+      .subscribe((res: any) => {
+        this.orderStatus = res.orderStatus;
+        if (this.orderStatus) {
+          this.closeOrder = true;
+        }
+      });
+
+    this.socket.on('orderStatus', (res: { orderStatus: boolean }) => {
+      this.orderStatus = res.orderStatus;
+      if (res.orderStatus) {
+        this.closeOrder = true;
+      } else {
+        this.closeOrder = false;
+      }
+    });
   }
 }
