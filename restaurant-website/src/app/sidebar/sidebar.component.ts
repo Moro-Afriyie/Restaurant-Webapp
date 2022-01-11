7ef2f72b81818b6c6f3a7e314b4f74e5cf387376
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-11 15:56:43 -0100

    add a logic to toggle the open and close orders button

--- restaurant-website/src/app/sidebar/sidebar.component.ts
+++ restaurant-website/src/app/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,13 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  orderStatus = false;
+  closeOrder = false;
+  private socket: any;
   constructor(
     private authService: AuthenticationService,
     private router: Router,
     private http: HttpClient
-  ) {}
+  ) {
+    this.socket = io('https://restaurant-payment-backend.herokuapp.com/');
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http
+      .get('https://restaurant-payment-backend.herokuapp.com/')
+      .subscribe((res: any) => {
+        this.orderStatus = res.orderStatus;
+        if (this.orderStatus) {
+          this.closeOrder = true;
+        }
+      });
+
+    this.socket.on('orderStatus', (res: { orderStatus: boolean }) => {
+      this.orderStatus = res.orderStatus;
+      if (res.orderStatus) {
+        this.closeOrder = true;
+      } else {
+        this.closeOrder = false;
+      }
+    });
+  }
 
   logOut(): void {
     this.authService
