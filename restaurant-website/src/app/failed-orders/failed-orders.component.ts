76e35d32173be82aa24859a2b3a8be417b89eaa0
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-13 08:47:24 -0100

    add the html, scss, and ts code to the filed orders component

--- restaurant-website/src/app/failed-orders/failed-orders.component.ts
+++ restaurant-website/src/app/failed-orders/failed-orders.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { OrderDetailsAdmin } from '../models/interface';
 
 @Component({
   selector: 'app-failed-orders',
   templateUrl: './failed-orders.component.html',
-  styleUrls: ['./failed-orders.component.scss']
+  styleUrls: ['./failed-orders.component.scss'],
 })
 export class FailedOrdersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  item$: Observable<OrderDetailsAdmin[]>;
+  constructor(private firestore: AngularFirestore) {
+    this.item$ = this.exampleGetCollection();
   }
 
+  success: boolean = false;
+
+  ngOnInit(): void {}
+
+  exampleGetCollection(): Observable<any> {
+    return this.firestore
+      .collection('orders', (orders) =>
+        orders
+          .where('completed', '==', false)
+          .where('orderPaid', '==', false)
+          .orderBy('date', 'desc')
+      )
+      .valueChanges({ idField: 'Id' });
+  }
+
+  onOrderDelivered(id: string, orderId: string): void {
+    if (window.confirm(`Are you sure you want to comfirm oder: ${orderId}?`)) {
+      this.updateOrder(id, { completed: true })
+        // .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+      this.success = true;
+    }
+  }
+
+  onCancelOrder(id: string, orderId: string) {
+    if (window.confirm(`Do you really want to delete oder: ${orderId}?`)) {
+      this.deleteOrder(id)
+        // .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  }
+
+  updateOrder(id: string, data: { completed: boolean }): Promise<void> {
+    return this.firestore.collection('orders').doc(id).update(data);
+  }
+
+  deleteOrder(id: string): Promise<void> {
+    return this.firestore.collection('orders').doc(id).delete();
+  }
 }
